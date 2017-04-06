2cb11722e87c1f3c38bbc416180cebe3fed17ee5
rushdi <rushdi@gmail.com>
2017-04-06 21:05:13 +0200

    code modified for connecting to database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,6 +2,27 @@ var bodyParser = require ('body-parser');
 
 var mongoose = require('mongoose');
 
+//connect to the database
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://test:test@ds153710.mlab.com:53710/test01');
+
+//create a schema - this is like a blueprint
+
+var  todoSchema = new  mongoose.Schema({
+		item:String
+});
+
+//craete todo model
+
+var Todo = mongoose.model('Todo' , todoSchema);
+
+var itemOne = Todo({item:'say hi to piash vai'}).save(function(err){
+
+	if (err) throw err;
+	console.log('item saved');
+});
+
 var data =  [{item:"get millk"},{item:"make tea"},{item:"serve  to piash  vai"}];
 
 var urlencodedParser =bodyParser.urlencoded({extended:false});
